d221c505b07f8cc1cce7869696ce38a03f089349
Fredrik Valdmanis <fredrik@valdmanis.com>
2016-11-18 19:28:54 +0100

    Ryddigere mal for lambda 🚀

--- src/lambda.js
+++ src/lambda.js
@@ -1,4 +1,4 @@
-e strict';
+use strict';
 
 console.log('Loading function');
 
@@ -18,41 +18,35 @@ const dynamo = new doc.DynamoDB();
  * DynamoDB API as a JSON body.
  */
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log("Received lambda event: ", JSON.stringify(event));
+    console.log("Mottok lambda-event: ", JSON.stringify(event));
 
     const done = (err, res) => {
         if (err) {
-            console.log("Error when " + event.httpMethod + "'ing: " + err);
+            console.log("Feil ved " + event.httpMethod + "'ing: " + err);
         } else {
-            console.log("Result when " + event.httpMethod + "'ing: " + res);
+            console.log("Resultat ved " + event.httpMethod + "'ing: " + res);
         }
         return callback(null, {
             statusCode: err ? '400' : '200',
             body: err ? err.message : JSON.stringify(res),
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*' // Denne byttes senere ut med origin til S3 Website
+                //'Access-Control-Allow-Origin': 'http://0.0.0.0:8000'
+                'Access-Control-Allow-Origin': 'http://serverless-bucketz.s3-website.eu-central-1.amazonaws.com'
             },
         })
     };
 
     switch (event.httpMethod) {
         case 'DELETE':
-            console.log(JSON.parse(event.body));
             dynamo.deleteItem({ TableName: "todos", Key: JSON.parse(event.body) }, done);
             break;
         case 'GET':
             dynamo.scan({ TableName: "todos" }, done);
             break;
         case 'POST':
-            console.log("Adding todo: " + event.body)
-            ///dynamo.putItem(({TableName: "testtabell", Item: {name: "Fredrik"}}), done);
             dynamo.putItem({ TableName: "todos", Item: JSON.parse(event.body) }, done)
             break;
-        //case 'PUT':
-        //    dynamo.updateItem(JSON.parse(event.body), done);
-            //break;
         default:
             done(new Error(`Ukjent HTTP-metode. Du sendte inn følgende greier:` + JSON.stringify(event)));
     }
